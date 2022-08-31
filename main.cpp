c799ba05469b06112c9cc854678c0e1c2331434d
Liuyuan <2787141886@qq.com>
2022-08-31 18:59:59 +0800

    update main.cpp for better debug and view

--- main.cpp
+++ main.cpp
@@ -1,27 +1,77 @@
 #include "Node.h"
-#if DEBUG
-#include "./Test/test_node.h"
-#endif
+
 #include <cstddef>
 #include <iostream>
 #include <memory>
 #include <utility>
 
-using namespace liuyuan;
+#if DEBUG
+	#ifdef TEST_NODE
+	#include "./Test/test_node.h"
+	#endif
+
+	#ifdef TEST_TREE
+	#include "./Test/test_node_tree.h"
+	#endif
+
+	#ifdef TEST_TOOL
+	#include "./Test/test_tool.h"
+	#endif
+
+	#define TEST_ARR(ARR_NAME) \
+		std::cout << #ARR_NAME << std::endl; \
+		for (size_t index = 0; index < sizeof(ARR_NAME) / sizeof(*ARR_NAME); index++) \
+		{ \
+			TEST_INFO result = ARR_NAME[index](); \
+			if (result.first == FAIL) \
+				std::cout << result.second << std::endl; \
+		} \
+		std::cout << std::endl;
+
+#endif
+
+#if DEBUG
+	using liuyuan::test::TEST_INFO;
+	using liuyuan::test::SUCCESS;
+	using liuyuan::test::FAIL;
+	#ifdef TEST_NODE
+		using liuyuan::test::test_arr_node;
+	#endif
+	#ifdef TEST_TREE
+		using liuyuan::test::test_arr_tree;
+	#endif
+	#ifdef TEST_TOOL
+		using liuyuan::test::test_arr_tool;
+	#endif
+#endif
 using std::cout;
 using std::endl;
 using std::shared_ptr;
 using std::pair;
+namespace liuyuan 
+{
+	void debug();
+}
 
 int main()
 {
-#if DEBUG
-	// liuyuan::test::TEST_INFO result = liuyuan::test::test_CST_DST_setters();
-	// liuyuan::test::TEST_INFO result = liuyuan::test::test_const_node();
-	// liuyuan::test::TEST_INFO result = liuyuan::test::test_insert_child_for_file_node();
-	
-	if (!result.first) { cout << result.second << endl; }
+#if !DEBUG
+#elif DEBUG
+	liuyuan::debug();
 #endif
 }
 
+void liuyuan::debug()
+{
+#ifdef TEST_NODE
+		TEST_ARR(test_arr_node)
+	#endif
 
+	#ifdef TEST_TREE
+		TEST_ARR(test_arr_tree)
+	#endif
+
+	#ifdef TEST_TOOL
+		TEST_ARR(test_arr_tool)
+#endif
+}
